fc743317b8e0833aa3cbafb8a77eff09efbdc23d
oussamahamzeh <hamzehoussama@gmail.com>
2024-01-08 14:18:57 +0100

    Commit002
    Login required pages now redirect to the login page.

--- InvMngSys/settings.py
+++ InvMngSys/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
 
 #LOGIN_REDIRECT_URL = '/index/'
+LOGIN_URL = '/admin/login/'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
